1404e823fca522b5f1039ac82018df9915524376
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-07-08 17:51:05 +1000

    Auto-generated commit

--- src/app/utils/date.ts
+++
@@ -1,7 +0,0 @@
-import * as moment from 'moment';
-
-export class DateFormats {
-  static formattedDate(date): string {
-    return moment(date).format("D MMM YYYY");
-  }
-}
--- src/lib/js-year-calendar/interfaces/CalendarRenderEndEventObject.ts
+++
@@ -1,6 +0,0 @@
-export default interface CalendarRenderEndEventObject {
-    /**
-     * The rendered year.
-     */
-    currentYear: number;
-}
