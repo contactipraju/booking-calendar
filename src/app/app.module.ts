547ebe380b3a4ccad5d2329f279b8a16b419b415
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-07-08 12:03:23 +1000

    Bootstraping: importing required modules from the libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule
     AppRoutingModule
   ],
   providers: [],
