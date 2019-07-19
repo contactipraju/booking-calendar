fb2d0a87b6ed23273489f8834ea2fb1520e7a73d
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-07-19 15:04:33 +1000

    Multiselect list: an entryComponent for listing bookings in selected dateRange

--- src/app/booking-calendar/booking-calendar.module.ts
+++ src/app/booking-calendar/booking-calendar.module.ts
@@ -11,6 +11,7 @@ import { TableViewComponent } from './table-view/table-view.component';
 import { YearCalendarComponent } from './year-calendar/year-calendar.component';
 import { YearTableComponent } from './year-table/year-table.component';
 import { EditBookingComponent } from './edit-booking/edit-booking.component';
+import { MultiselectListComponent } from './multiselect-list/multiselect-list.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { EditBookingComponent } from './edit-booking/edit-booking.component';
     TableViewComponent,
     YearCalendarComponent,
     YearTableComponent,
-    EditBookingComponent
+    EditBookingComponent,
+    MultiselectListComponent
   ],
   imports: [
     CommonModule,
@@ -31,7 +33,7 @@ import { EditBookingComponent } from './edit-booking/edit-booking.component';
   exports: [
     BookingCalendarComponent
   ],
-  entryComponents: [EditBookingComponent],
+  entryComponents: [EditBookingComponent, MultiselectListComponent],
   providers: [BookingService]
 })
 export class BookingCalendarModule { }
---
+++ src/app/booking-calendar/multiselect-list/multiselect-list.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit }          from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+import { EditBookingComponent}        from '../edit-booking/edit-booking.component';
+
+@Component({
+  selector: 'app-multiselect-list',
+  templateUrl: './multiselect-list.component.html',
+  styleUrls: ['./multiselect-list.component.sass']
+})
+export class MultiselectListComponent implements OnInit {
+  list: any;
+  selectedBookingId: number;
+
+  modalRef:  BsModalRef;
+
+  constructor(private modalService: BsModalService) { }
+
+  ngOnInit() {
+    this.selectedBookingId = this.list[0].id;
+  }
+
+  getBookingFromId(id) {
+    console.log('MultiselectListComponent - getBookingFromId: ', id);
+
+    for(let i=0; i<this.list.length; i++) {
+      if(id == this.list[i].id) {
+        return this.list[i];
+      }
+    }
+
+    return null;
+  }
+
+  onSelectBooking() {
+    console.log('MultiselectListComponent - onSelectBooking: ', this.selectedBookingId);
+    let selectedBooking = this.getBookingFromId(this.selectedBookingId);
+
+    if(!selectedBooking) {
+      return;
+    }
+
+    const initialState = {
+      mode: "edit",
+      booking: selectedBooking
+    };
+
+    this.modalRef = this.modalService.show(EditBookingComponent, { initialState });
+    this.modalRef.content.modalRef = this.modalRef;
+  }
+}
