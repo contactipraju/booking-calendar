76917049ba09b6168481793d8a254a9073250007
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-07-25 11:12:09 +1000

    Minor cleanup; no functional changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+/* Angular Modules */
+import { NgModule }                from '@angular/core';
+import { FormsModule }             from '@angular/forms';
+import { RouterModule }            from '@angular/router';
+import { BrowserModule }           from '@angular/platform-browser';
+import { HttpClientModule }        from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import { BookingCalendarModule } from './booking-calendar/booking-calendar.module';
-import { AppComponent } from './app.component';
+/* Third-party Modules */
+import { ModalModule }             from 'ngx-bootstrap/modal';
+
+/* Routing Module */
+import { AppRoutingModule }        from './app-routing.module';
+
+/* App Modules */
+import { BookingCalendarModule }   from './booking-calendar/booking-calendar.module';
+
+/* App Component */
+import { AppComponent }            from './app.component';
 
 @NgModule({
   declarations: [
