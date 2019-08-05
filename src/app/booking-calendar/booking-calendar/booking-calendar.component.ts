dd68377f5ed27ff8c8481ff9a869e2290486ffb0
Prasada Indukuri <prasadaindukuri@ahoyclub.com>
2019-08-05 17:55:47 +1000

    Using ngrx to get and pass bookings data to sub-components

--- src/app/booking-calendar/booking-calendar/booking-calendar.component.ts
+++ src/app/booking-calendar/booking-calendar/booking-calendar.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit }  from '@angular/core';
 import { ActivatedRoute }     from '@angular/router';
 
+import { Store, select }      from '@ngrx/store';
+
+import { IAppState }          from './../../store/state/app.state';
+import { GetBookings }        from './../../store/actions/booking.actions';
+import { selectBookingList }  from './../../store/selectors/booking.selectors';
 
 @Component({
   selector: 'bc-booking-calendar',
@@ -8,9 +13,15 @@ import { ActivatedRoute }     from '@angular/router';
   styleUrls: ['./booking-calendar.component.scss']
 })
 export class BookingCalendarComponent implements OnInit {
-  constructor() {
+  bookings$ = this._store.pipe(select(selectBookingList));
+
+  constructor(
+    private _store: Store<IAppState>,
+    private activatedRoute: ActivatedRoute
+    ) {
   }
 
   ngOnInit() {
+    this._store.dispatch(new GetBookings(this.activatedRoute.snapshot.params.id));
   }
 }
